acc25e6ad19f135d5c15d0729588925a59638b26
exolop <80510724+Xlaaf@users.noreply.github.com>
2021-07-14 16:25:56 +0700

    Update config.py

--- NekozuUserbotPy/config.py
+++ NekozuUserbotPy/config.py
@@ -1,7 +1,6 @@
 class Config(object):
     api_id = "4728859"
     api_hash = "a7391b53a4bb7838dc950f0b865ad43f"
-    session = "BQBNKOmmuziBZHZJCmMPOhfQZz4BQ7uRZqKrBgHPlXYDOgE-Z1DWiMLQoeFV8u_1cV9xfE5IMVFeRYq67OPD3JZX4M5aufx_LHR-871RNAyrb6-0Oe-MAgWNpK6ztt4rquRShYWkxC3t1cf54h88CgJkfgGO6qy3CRKTcx_VS6pXeZ5GWBl9RV8lNqSYSuPwf4yt38BElnC9ZKIy9HUuZVTYdwmbndI3JGH4sJji5qa6Q8w5gUoXw6JEzcyUCL105D6fwmNcOtzuTgFGZos1cvUAPY3xnlPRfwZnDnP3s60I1krlka_HX4RCxZwPq_eMpiPrLScE5NlnAeGEzeixc8ZScQr62AA
-"
+    session = "BQBNKOmmuziBZHZJCmMPOhfQZz4BQ7uRZqKrBgHPlXYDOgE-Z1DWiMLQoeFV8u_1cV9xfE5IMVFeRYq67OPD3JZX4M5aufx_LHR-871RNAyrb6-0Oe-MAgWNpK6ztt4rquRShYWkxC3t1cf54h88CgJkfgGO6qy3CRKTcx_VS6pXeZ5GWBl9RV8lNqSYSuPwf4yt38BElnC9ZKIy9HUuZVTYdwmbndI3JGH4sJji5qa6Q8w5gUoXw6JEzcyUCL105D6fwmNcOtzuTgFGZos1cvUAPY3xnlPRfwZnDnP3s60I1krlka_HX4RCxZwPq_eMpiPrLScE5NlnAeGEzeixc8ZScQr62AA"
     prefix = "."
     alive_img = "https://telegra.ph/file/5edfa2bf5ecb8f8105527.jpg"
