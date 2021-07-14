daed246ba56f30365131612613f23e09dc8c8460
exolop <80510724+Xlaaf@users.noreply.github.com>
2021-07-14 16:03:36 +0700

    Update __init__.py

--- NekozuUserbotPy/__init__.py
+++ NekozuUserbotPy/__init__.py
@@ -2,11 +2,9 @@ import logging
 import sys
 import time
 from pyrogram import Client, errors
-from dotenv import load_dotenv
 import logging
 import os
-
-load_dotenv("config.env")
+from NekozuUserbotPy.config import Config
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -16,10 +14,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 StartTime = time.time()
 
-API_ID = int(os.environ.get("api_id") or 0)
-API_HASH = str(os.environ.get("api_hash") or None)
-SESSION = os.environ.get("session", "")
-PREFIX = os.environ.get("prefix")
-ALIVE_IMG = os.environ.get("alive_img")
+API_ID = Config.api_id
+API_HASH = Config.api_hash
+SESSION = Config.session
+PREFIX = Config.prefix
+ALIVE_IMG = Config.alive_img
 
 xo = Client(SESSION, api_id=API_ID, api_hash=API_HASH)
