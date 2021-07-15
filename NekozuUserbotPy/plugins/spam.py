543e396e4efa2f7657234d256d2bde685ff10c59
exolop <80510724+Xlaaf@users.noreply.github.com>
2021-07-15 08:23:16 +0700

    Create spam.py

---
+++ NekozuUserbotPy/plugins/spam.py
@@ -0,0 +1,17 @@
+from pyrogram import filters
+from pyrogram.errors import FloodWait
+from NekozuUserbotPy import xo, PREFIX
+import asyncio
+
+@xo.on_message(filters.command("spam", PREFIX) & filters.me)
+async def spam(Client, message):
+    x = message.text
+    y = x.split(" ")
+    spam_count = int(y[1])
+    spam_text = str(y[2:])
+    try:
+        while spam_count > 0:
+            await xo.send_message(message.chat.id, spam_text)
+            spam_count -= 1
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
