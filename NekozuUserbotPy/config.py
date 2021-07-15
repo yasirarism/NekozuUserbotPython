fe4950bd17dfbef18840e81622da4e8c900d0c81
exolop <80510724+Xlaaf@users.noreply.github.com>
2021-07-15 08:28:17 +0700

    Update and rename config.py to sample_config.py

--- NekozuUserbotPy/config.py
+++
@@ -1,6 +0,0 @@
-class Config(object):
-    api_id = "Api Id"
-    api_hash = "Api Hash"
-    session = "String session"
-    prefix = "."
-    alive_img = "Alive img"
---
+++ NekozuUserbotPy/sample_config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config(object):
+    api_id = int(os.environ.get("api_id", 12345))
+    api_hash = os.environ.get("API_HASH", None)
+    session = os.environ.get("STRING_SESSION", None)
+    prefix = os.environ.get(".", None)
+    alive_img = os.environ.get("alive img", None)
