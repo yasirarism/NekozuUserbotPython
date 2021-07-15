cae8fcc9084bc0ac8032b6c928663b0b782668d2
exolop <80510724+Xlaaf@users.noreply.github.com>
2021-07-15 10:16:35 +0700

    Update google.py

--- NekozuUserbotPy/plugins/google.py
+++ NekozuUserbotPy/plugins/google.py
@@ -1,39 +1,73 @@
-# Credit userge 
-
-from search_engine_parser.core.engines.google import Search as GoogleSearch
-from pyrogram.types import Message
+# Credit Usege team https://github.com/usergeteam/userge
+import re
+import urllib
+import urllib.parse
 from pyrogram import filters
-from NekozuUserbotPy import xo, PREFIX
+from pyrogram.types import Message
+from fake_useragent import UserAgent
+
+import requests
+from bs4 import BeautifulSoup
+from NekozuUserbotPy import PREFIX, xo
+
+def get_text(message: Message) -> [None, str]:
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
 
 @xo.on_message(filters.command("google", PREFIX) & filters.me)
-async def gsearch(message: Message):
-    await message.edit("Proses ...")
-    query = message.filtered_input_str
-    flags = message.flags
-    page = int(flags.get("-p", 1))
-    limit = int(flags.get("-l", 5))
-    if message.reply_to_message:
-        query = message.reply_to_message.text
+async def gsrch(_, message: Message):
+    pablo = await message.edit("`Processing..`")
+    query = get_text(message)
     if not query:
-        await message.err(text="Berikan pertanyaan atau balas pesan!")
+        await message.edit(
+            "`Tolong Beri Saya Masukan yang Valid. Anda Dapat Memeriksa Menu Bantuan Untuk Mengetahui Lebih Lanjut!`"
+        )
         return
-    try:
-        g_search = GoogleSearch()
-        gresults = await g_search.async_search(query, page)
-    except Exception as e:
-        await message.err(text=e)
-        return
-    output = ""
-    for i in range(limit):
-        try:
-            title = gresults["titles"][i]
-            link = gresults["links"][i]
-            desc = gresults["descriptions"][i]
-            output += f"[{title}]({link})\n"
-            output += f"`{desc}`\n\n"
-        except IndexError:
-            break
-    output = f"**Pencarian Google:**\n`{query}`\n\n**Hasil:**\n{output}"
-    await message.edit_or_send_as_file(
-        text=output, caption=query, disable_web_page_preview=True
+    query = urllib.parse.quote_plus(query)
+    number_result = 8
+    ua = UserAgent()
+    google_url = (
+        "https://www.google.com/search?q=" + query + "&num=" + str(number_result)
     )
+    response = requests.get(google_url, {"User-Agent": ua.random})
+    soup = BeautifulSoup(response.text, "html.parser")
+    result_div = soup.find_all("div", attrs={"class": "ZINbbc"})
+    links = []
+    titles = []
+    descriptions = []
+    for r in result_div:
+        try:
+            link = r.find("a", href=True)
+            title = r.find("div", attrs={"class": "vvjwJb"}).get_text()
+            description = r.find("div", attrs={"class": "s3v9rd"}).get_text()
+            if link != "" and title != "" and description != "":
+                links.append(link["href"])
+                titles.append(title)
+                descriptions.append(description)
+
+        except:
+            continue
+    to_remove = []
+    clean_links = []
+    for i, l in enumerate(links):
+        clean = re.search("\/url\?q\=(.*)\&sa", l)
+        if clean is None:
+            to_remove.append(i)
+            continue
+        clean_links.append(clean.group(1))
+    for x in to_remove:
+        del titles[x]
+        del descriptions[x]
+    msg = ""
+
+    for tt, liek, d in zip(titles, clean_links, descriptions):
+        msg += f"[{tt}]({liek})\n`{d}`\n\n"
+    await message.edit("**Pencarian:**\n`" + query + "`\n\n**Hasil:**\n" + msg)
