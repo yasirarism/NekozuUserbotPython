6345dd0265902722c5bc818ad58d7f8d9fa8f429
exolop <80510724+Xlaaf@users.noreply.github.com>
2021-07-15 09:33:24 +0700

    Update config.py

--- NekozuUserbotPy/config.py
+++ NekozuUserbotPy/config.py
@@ -1,6 +1,8 @@
-class Config(object):
-    api_id = api id
-    api_hash = "api hash"
-    session = "string session"
-    prefix = "."
-    alive_img = " alive img"
+import os
+
+class Config:
+    api_id = int(os.environ.get("api_id", 12345))
+    api_hash = os.environ.get("API_HASH", None)
+    session = os.environ.get("STRING_SESSION", None)
+    prefix = os.environ.get(".", None)
+    alive_img = os.environ.get("alive img", None)
