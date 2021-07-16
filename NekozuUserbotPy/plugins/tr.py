d00aa7461d16dc4aa96400b8bd7daa1f745f87f5
exolop <80510724+Xlaaf@users.noreply.github.com>
2021-07-16 07:04:31 +0700

    Update tr.py

--- NekozuUserbotPy/plugins/tr.py
+++ NekozuUserbotPy/plugins/tr.py
@@ -1,28 +1,15 @@
-# This file is Originally Written By @okay-retard on GitHub
-# The Author (Jayant Kageri) just Ported this for Devloper Userbot
-# (C) 2021 Jayant Kageri
-# I Just Take This. Original Author Is Above
-
-from google_trans_new import google_translator
+from googletrans import Translator
 from pyrogram import filters
-from inspect import getfullargspec
-from pyrogram.types import Message
-from NekozuUserbotPy import xo, PREFIX
-trl = google_translator()
 
-async def edrep(msg: Message, **kwargs):
-    func = msg.edit_text if msg.from_user.is_self else msg.reply
-    spec = getfullargspec(func.__wrapped__).args
-    await func(**{k: v for k, v in kwargs.items() if k in spec})
+from nana import xo, PREFIX
 
+trl = Translator()
 
 @xo.on_message(filters.command("tr", PREFIX) & filters.me)
 async def translate(_client, message):
-    if message.reply_to_message and (
-        message.reply_to_message.text or message.reply_to_message.caption
-    ):
+    if message.reply_to_message and (message.reply_to_message.text or message.reply_to_message.caption):
         if len(message.text.split()) == 1:
-            await edrep(message, text="Usage: Reply to a message, then `tr <lang>`")
+            await message.edit("Pemakaian: Balas pesan, lalu `tr <bahasa>`")
             return
         target = message.text.split()[1]
         if message.reply_to_message.text:
@@ -31,24 +18,21 @@ async def translate(_client, message):
             text = message.reply_to_message.caption
         detectlang = trl.detect(text)
         try:
-            tekstr = trl.translate(text, lang_tgt=target)
+            tekstr = trl.translate(text, dest=target)
         except ValueError as err:
-            await edrep(message, text=f"Error: `{str(err)}`")
+            await message.edit("Kesalahan: `{}`".format(str(err)))
             return
+        await message.edit("Diterjemahkan dari `{}` to `{}`:\n```{}```".format(detectlang.lang, target, tekstr.text))
     else:
         if len(message.text.split()) <= 2:
-            await edrep(message, text="Usage: `tr <lang> <text>`")
+            await message.edit("Penggunaan: `tr <bahasa> <teks>`")
             return
         target = message.text.split(None, 2)[1]
         text = message.text.split(None, 2)[2]
         detectlang = trl.detect(text)
         try:
-            tekstr = trl.translate(text, lang_tgt=target)
+            tekstr = trl.translate(text, dest=target)
         except ValueError as err:
-            await edrep(message, text="Error: `{}`".format(str(err)))
+            await message.edit("Error: `{}`".format(str(err)))
             return
-
-    await edrep(
-        message,
-        text=f"Translated from `{detectlang[0]}` to `{target}`:\n```{tekstr}```",
-    )
+        await message.edit("Diterjemahkan dari `{}` ke `{}`:\n```{}```".format(detectlang.lang, target, tekstr.text))
