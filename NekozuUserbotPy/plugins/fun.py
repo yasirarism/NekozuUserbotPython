9616ee0694906fd18de118279c47d06bdca8855e
nekozu <81870902+nekozu@users.noreply.github.com>
2021-07-17 21:04:54 +0700

    Create fun.py

---
+++ NekozuUserbotPy/plugins/fun.py
@@ -0,0 +1,15 @@
+import requests
+from pyrogram import filters
+from pyrogram.types import Message
+from NekozuUserbotPy import xo, PREFIX
+
+
+@xo.on_message(filters.command("br", PREFIX) & filters.me)
+async def meme(_, message: Message):
+    meme = r.get("https://meme-api.herokuapp.com/gimme/Animemes/").json()
+    image = meme.get("url")
+    caption = meme.get("title")
+    if not image:
+        msg.reply_text("No URL was received from the API!")
+        return
+    xo.send_photo(meme.chat.id, image, caption=caption)
