6f7069493a38839e7c62ab5d159fc31dafc6b42a
nekozu <81870902+nekozu@users.noreply.github.com>
2021-07-17 13:39:17 +0700

    Create brainly.py

---
+++ NekozuUserbotPy/plugins/brainly.py
@@ -0,0 +1,28 @@
+import requests
+from pyrogram import filters
+from pyrogram.types import Message
+from NekozuUserbotPy import xo, PREFIX
+
+ses = requests.session()
+
+def get_text(message: Message) -> [None, str]:
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
+
+@xo.on_message(filters.command("br", PREFIX) & filters.me)
+async def brainly(_, message: Message):
+    query = get_text(message)
+     url = "https://pencarikode.xyz/api/brainly?search="+query+"&apikey=APIKEY"
+        hasil = ses.get(url).json()
+        if hasil['status'] == 200:
+            brainly = hasil['data']['pertanyaan']
+            beh = hasil['data']['jawaban']
+            await message.edit("*Soal**\n`"+brainly+"`\n\n**Jawaban:**\n"+beh)
