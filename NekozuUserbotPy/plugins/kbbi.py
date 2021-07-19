922d48ddcc0b521d7d97264a5dcb269ebcdfb6ef
nekozu <81870902+nekozu@users.noreply.github.com>
2021-07-19 20:05:41 +0700

    Update kbbi.py

--- NekozuUserbotPy/plugins/kbbi.py
+++ NekozuUserbotPy/plugins/kbbi.py
@@ -7,9 +7,9 @@ from NekozuUserbotPy.plugins.brainly import get_text
 ses = requests.session()
 
 @xo.on_message(filters.command("kbbi", PREFIX) & filters.me)
-async def brainly(_, message: Message):
+async def kbbi(_, message: Message):
     query = get_text(message)
     url = "https://kbbi-api-zhirrr.vercel.app/api/kbbi?text="+query
     hasil = ses.get(url).json()
     kbbi = hasil['arti']
-    await message.edit("Kata: "+query+"\nArti: "+kbbi)
+    await message.edit("**Kata:** `"+query+"`\n__Arti__: "+kbbi)
