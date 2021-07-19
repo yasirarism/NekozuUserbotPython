68d652c13b73e3573ad1051049be218eeacbc1f5
nekozu <81870902+nekozu@users.noreply.github.com>
2021-07-19 20:19:18 +0700

    Update kbbi.py

--- NekozuUserbotPy/plugins/kbbi.py
+++ NekozuUserbotPy/plugins/kbbi.py
@@ -1,15 +1,27 @@
 import requests
 from pyrogram import filters
 from pyrogram.types import Message
-from NekozuUserbotPy import xo, PREFIX, XTEAM_API
-from NekozuUserbotPy.plugins.brainly import get_text
+from NekozuUserbotPy import xo, PREFIX
 
 ses = requests.session()
+
+def get_text(message: Message) -> [None, str]:
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
 
 @xo.on_message(filters.command("kbbi", PREFIX) & filters.me)
 async def kbbi(_, message: Message):
     query = get_text(message)
     url = "https://kbbi-api-zhirrr.vercel.app/api/kbbi?text="+query
     hasil = ses.get(url).json()
-    kbbi = hasil['arti']
-    await message.edit('Kata:'+query+'\n__Arti__:'+kbbi)
+    kj = hasil['arti']
+    jk = hasil['lema']
+    await message.edit("**Lema**\n`"+jk+"`\n\n**Arti:**\n"+kj)
