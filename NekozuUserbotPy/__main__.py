384b2614227d056b810340c18025e3dd5a398ae2
exolop <80510724+Xlaaf@users.noreply.github.com>
2021-07-22 11:08:11 +0700

    Create __main__.py

---
+++ NekozuUserbotPy/__main__.py
@@ -0,0 +1,17 @@
+from pyrogram import idle, Client
+from NekozuUserbotPy import xo
+from NekozuUserbotPy.plugins import (
+    atmim,
+    alive,
+    help,
+    google,
+    brainly,
+    fun,
+    github,
+    pinpon,
+    telegraph,
+)
+
+
+xo.start()
+idle()
